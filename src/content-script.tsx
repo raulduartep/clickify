032a6ce4489ffdadd1dcdd68df8690975e060875
Raul Duarte Pereira <raulduartepe@gmail.com>
2023-11-11 21:27:13 -0300

    first commit

---
+++ src/content-script.tsx
@@ -0,0 +1,15 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+
+const root = document.createElement("div");
+root.id = "crx-root";
+document.body.appendChild(root);
+
+console.log("Hello from content script!");
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
