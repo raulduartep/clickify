3267eabadd51737b813700a528f10165d0b0d7e0
Raul Duarte Pereira <raulduartepe@gmail.com>
2024-02-23 18:31:52 -0300

    bug: fix env validation error

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,9 +1,18 @@
-import { z } from "zod"
+import { z } from 'zod'
 
 const envSchema = z.object({
-  VITE_TEST_CLICKUP_TASK_ID: z.string(),
-  VITE_TEST_CLICKUP_TASK_NAME: z.string(),
-  VITE_TEST_CLICKUP_TASK_PROJECT_NAME: z.string(),
+  VITE_TEST_CLICKUP_TASK_ID: z
+    .string()
+    .optional()
+    .transform(val => val ?? ''),
+  VITE_TEST_CLICKUP_TASK_NAME: z
+    .string()
+    .optional()
+    .transform(val => val ?? ''),
+  VITE_TEST_CLICKUP_TASK_PROJECT_NAME: z
+    .string()
+    .optional()
+    .transform(val => val ?? ''),
   DEV: z.boolean(),
 })
 
